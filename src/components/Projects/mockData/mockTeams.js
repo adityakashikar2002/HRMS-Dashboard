84e553290feda6d23d22cbab8a0f3358dc80498e
adityakashikar2002 <adityakashikar02@gmail.com>
2025-04-07 16:58:11 +0530

    mockdata changes

--- src/components/Projects/mockData/mockTeams.js
+++ src/components/Projects/mockData/mockTeams.js
@@ -1,32 +1,3 @@
-// import { mockMembers } from './mockMembers';
-
-// export const mockTeams = [
-//   {
-//     id: '1',
-//     name: 'Frontend Team',
-//     description: 'Responsible for all frontend development',
-//     members: [mockMembers[0], mockMembers[1]],
-//     createdAt: '2023-01-15'
-//   },
-//   {
-//     id: '2',
-//     name: 'Design Team',
-//     description: 'UI/UX design and prototyping',
-//     members: [mockMembers[2]],
-//     createdAt: '2023-02-20'
-//   },
-//   {
-//     id: '3',
-//     name: 'Backend Team',
-//     description: 'Server-side development and APIs',
-//     members: [mockMembers[1], mockMembers[4]],
-//     createdAt: '2023-03-10'
-//   }
-// ];
-
-
-
-
 import { mockMembers } from './mockMembers';
 
 export const mockTeams = [
--- src/components/Projects/utils/projectStorage.js
+++ src/components/Projects/utils/projectStorage.js
@@ -1,31 +1,3 @@
-// const PROJECTS_KEY = 'projects';
-
-// export const getProjects = () => {
-//   const projects = localStorage.getItem(PROJECTS_KEY);
-//   return projects ? JSON.parse(projects) : [];
-// };
-
-// export const saveProject = (project) => {
-//   const projects = getProjects();
-//   const existingIndex = projects.findIndex(p => p.id === project.id);
-  
-//   if (existingIndex >= 0) {
-//     projects[existingIndex] = project;
-//   } else {
-//     projects.push(project);
-//   }
-  
-//   localStorage.setItem(PROJECTS_KEY, JSON.stringify(projects));
-// };
-
-// export const deleteProject = (projectId) => {
-//   const projects = getProjects().filter(p => p.id !== projectId);
-//   localStorage.setItem(PROJECTS_KEY, JSON.stringify(projects));
-// };
-
-// export const getProjectById = (projectId) => {
-//   return getProjects().find(p => p.id === projectId);
-// };
 // projectStorage.js
 const PROJECTS_KEY = 'projects';
 
