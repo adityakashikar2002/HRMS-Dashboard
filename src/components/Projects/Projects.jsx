722165009750b556fc79cb60b6c6cee48f1bb3c1
adityakashikar2002 <adityakashikar02@gmail.com>
2025-04-07 16:02:47 +0530

    project changes

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import ProjectsList from './pages/ProjectsList/ProjectsList';
-// import TeamsList from './pages/TeamsList/TeamsList';
-// import ProjectDetail from './pages/ProjectDetail/ProjectDetail';
-
-// const Projects = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<ProjectsList />} />
-//       <Route path="/teams" element={<TeamsList />} />
-//       <Route path="/:projectId" element={<ProjectDetail />} />
-//     </Routes>
-//   );
-// };
-
-// export default Projects;
-
-
 // Projects.jsx
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
